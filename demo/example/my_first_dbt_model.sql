ca8011bb97f4556a035ced9026db835f02d2f901
Sarah Morris <srmorris@g.syr.edu>
2024-11-16 21:53:09 +0000

    add customers model, tests, docs

--- demo/example/my_first_dbt_model.sql
+++ demo/example/my_first_dbt_model.sql
@@ -7,13 +7,13 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
 with source_data as (
 
     select 1 as id
     union all
-    select null as id
+    select 1 as id
 
 )
 
@@ -24,4 +24,4 @@ from source_data
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+where id is not null
